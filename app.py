a33790142ba2572c6ad640470bf9bff5b453ba27
Fardeen Khan <fk7703890@gmail.com>
2026-06-22 19:12:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pickle
+import numpy as np
 import pandas as pd
+import time
+
 # ---------------------------------------------
 # 1. PAGE CONFIGURATION & THEME
 # ---------------------------------------------
